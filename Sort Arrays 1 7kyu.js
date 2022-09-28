55975e7593efeff3d32f580a752011a0669bd5fb
cheekygerold <spanishoceango@gmail.com>
2022-09-27 19:56:07 -0700

    add daily challenge

---
+++ Sort Arrays 1 7kyu.js
@@ -0,0 +1,6 @@
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+// answer
+
+
+let sortme = ( names ) => names.sort((a,b)=> a>b?1:-1)
