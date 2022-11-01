a7143835c76fbc321ec8f6c9b4992522f648ccac
cheekygerold <spanishoceango@gmail.com>
2022-10-31 20:45:16 -0700

    add daily challenge

---
+++ shorter concat reverse longer 7kyu.js
@@ -0,0 +1,14 @@
+// Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
+
+// In other words, the shortest string has to be put as prefix and as suffix of the reverse of the longest.
+
+// Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
+// If a and b have the same length treat a as the longer producing
+
+//answer
+
+function shorter_reverse_longer(a,b){
+    let shorter = a.split('').length < b.split('').length ? a : b
+    let longer =  a.split('').length >= b.split('').length ? a : b 
+    return shorter + longer.split('').reverse().join('') +shorter
+  }
