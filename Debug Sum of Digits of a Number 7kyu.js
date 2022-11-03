7b6761e016909a17ed232e901df57699bb395cba
cheekygerold <spanishoceango@gmail.com>
2022-11-02 17:55:00 -0700

    add daily challenge

---
+++ Debug Sum of Digits of a Number 7kyu.js
@@ -0,0 +1,12 @@
+// Debug   function getSumOfDigits that takes positive integer to calculate sum of it's digits. Assume that argument is an integer.
+
+//answer
+
+function getSumOfDigits(integer) {
+    var sum = null;
+    var digits =  Math.floor(integer).toString().split('');
+    for(var ix = 0; ix < digits.length; ix++) {
+      sum +=  +digits[ix];
+    }
+    return sum;
+  }
