3da294566e203dced703b0eeec7924b4f5a38a0c
cheekygerold <spanishoceango@gmail.com>
2023-03-18 21:54:17 -0700

    add daily challenge

---
+++ Add Length.js
@@ -0,0 +1,8 @@
+// Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
+
+// Note: String will have at least one element; words will always be separated by a space.
+
+function addLength(str) {
+    let arr = str.split(' ')
+   return arr.map((x,i)=> x=x+' '+x.length)
+   }
