e2dd9c572f36a6b3af22bd08e0f545808c6f3178
cheekygerold <spanishoceango@gmail.com>
2023-05-20 19:08:44 -0700

    add daily challenge

---
+++ Calculate average 8kyu.js
@@ -0,0 +1,8 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+function findAverage(array) {
+    // your code here
+    return !array.length ? 0 : array.reduce((x,i)=> x+=i)/array.length;
+  }
