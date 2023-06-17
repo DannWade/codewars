f4b40a02d5ffd46aa63fe78c2a603c82a530d70e
cheekygerold <spanishoceango@gmail.com>
2023-06-16 19:53:56 -0700

    add daily challenge

---
+++ React Docs updating arrays in state 1.js
@@ -0,0 +1,57 @@
+// Fill in the handleIncreaseClick logic so that pressing ”+” increases the corresponding number:
+
+
+import { useState } from 'react';
+
+const initialProducts = [{
+  id: 0,
+  name: 'Baklava',
+  count: 1,
+}, {
+  id: 1,
+  name: 'Cheese',
+  count: 5,
+}, {
+  id: 2,
+  name: 'Spaghetti',
+  count: 2,
+}];
+
+export default function ShoppingCart() {
+  const [
+    products,
+    setProducts
+  ] = useState(initialProducts)
+
+  function handleIncreaseClick(productId) {
+    const next = products.map((x,i)=>{
+      if(x.id == productId){
+        return{
+          ...x,
+          count:x.count+1,
+        }
+      }else{
+        return{
+          ...x
+        }
+      }
+    })
+    setProducts(next)
+  }
+  return (
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>
+          {product.name}
+          {' '}
+          (<b>{product.count}</b>)
+          <button onClick={() => {
+            handleIncreaseClick(product.id);
+          }}>
+            +
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
