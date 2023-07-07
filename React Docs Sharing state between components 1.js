6c50f5844f1c07f81c47afec220dfacd582313ec
cheekygerold <spanishoceango@gmail.com>
2023-07-06 19:35:29 -0700

    add daily challenge

---
+++ React Docs Sharing state between components 1.js
@@ -0,0 +1,27 @@
+// These two inputs are independent. Make them stay in sync: editing one input should update the other input with the same text, and vice versa.
+
+import { useState } from 'react';
+
+export default function SyncedInputs() {
+  const [text, setText] = useState('');
+  return (
+    <>
+      <Input label="First input" text={text} handleChange={(e)=>setText(e.target.value)} />
+      <Input label="Second input" text={text} handleChange={(e)=>setText(e.target.value)} />
+    </>
+  );
+}
+
+function Input({ label,text='hello',handleChange }) {
+
+  return (
+    <label>
+      {label}
+      {' '}
+      <input
+        value={text}
+        onChange={handleChange}
+      />
+    </label>
+  );
+}
