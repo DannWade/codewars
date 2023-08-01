d82d67bf04e63fb14b422219ac70fe4240be54dc
cheekygerold <spanishoceango@gmail.com>
2023-07-31 19:53:22 -0700

    add daily challenge

---
+++ Triangular Treasure 7kyu.js
@@ -0,0 +1,6 @@
+// You need to return the nth triangular number. You should return 0 for out of range values:
+
+// Return the nth triangular number
+function triangular( n ) {
+    return n <=0 ? 0 : ((n*(n+1))/2)
+  }
