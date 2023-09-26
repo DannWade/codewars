e89d081e957e3d6a9965f976cfd331c6516e0065
cheekygerold <spanishoceango@gmail.com>
2023-09-25 20:17:52 -0700

    add daily challenge

---
+++ Is it a palindrome 8kyu.js
@@ -0,0 +1,9 @@
+// Write a function that checks if a given string (case insensitive) is a palindrome.
+
+// A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
+
+function isPalindrome(x) {
+    let lower = x.toLowerCase()
+    let palin = lower.split('').reverse().join('')
+    return palin == lower? true : false
+  }
