2a6e0f871edd39bf29ca9c61cd24d834d32e681a
cheekygerold <spanishoceango@gmail.com>
2024-02-14 20:39:09 -0800

    add daily challenge

---
+++ Grasshopper Combine strings 8kyu.js
@@ -0,0 +1,9 @@
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// combineNames('James', 'Stevens')
+
+function combineNames(f,l){
+    return f+" "+l
+  }
